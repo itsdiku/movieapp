0e25e68672913156389088180eb156af86d6c501
itsdiku <dikudas46@gmail.com>
2023-01-24 03:16:33 +0530

    Adding dist

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import "./App.css";
 import SearchIcon from "./assets/search.svg";
 import MovieCard from "./components/MovieCard";
 
-const API_URL = `http://www.omdbapi.com/?apikey=${
-  import.meta.env.VITE_API_KEY
-}`;
+// const API_URL = `https://www.omdbapi.com/?apikey=${
+//   import.meta.env.VITE_API_KEY
+// }`;
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setsearchTerm] = useState("");
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title})`);
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${
+        import.meta.env.VITE_API_KEY
+      }&s=${title})`
+    );
     const data = await response.json();
 
     setMovies(data.Search);
